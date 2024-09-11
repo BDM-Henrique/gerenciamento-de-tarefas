aec786e37700b0207ccb31d7df6d4164c2e4289c
Henrique <henrique1jose@gmail.com>
2024-09-11 17:51:41 -0300

    Inserindo o delete, atribuindo valores na lista de tarefas e deixanado elas listadas, e melhor uso dos States

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,43 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId){
+    const newTasks = tasks.map(task =>{
+      if (task.id === taskId){
+        return {...task, isCompleted: !task.isCompleted}
+      } else{
+        return task
+      }
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId){
+    const newTask = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTask); 
+
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask])
+  }
+
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] ">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
         {/* agora irei renderizar meu componente que acabei de criar */}
-        <AddTask />
-        <Tasks tasks={tasks }/>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks 
+        tasks={tasks} 
+        onTaskClick={onTaskClick} 
+        onDeleteTaskClick={onDeleteTaskClick}/>
       </div>
     </div>
   );
