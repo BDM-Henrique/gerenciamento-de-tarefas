f22d66a0e4483e8ae3339057854433916e40834c
Henrique <henrique1jose@gmail.com>
2024-09-18 11:33:38 -0300

    Criando componentes para Input, Button e Title

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
+import Input from "./Input"
 
 function AddTask({onAddTaskSubmit}) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     return(
         <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-            <input type="text" 
-            className="border border-slate-400 outline-slate-400 px-4 py-2 rounded-md" placeholder="Digite o título da tarefa"
-             value={title} 
-             onChange={(event) => setTitle(event.target.value)}/>
+            <Input 
+            type="text" 
+            placeholder="Digite o título da tarefa"
+            value={title} 
+            onChange={(event) => setTitle(event.target.value)}/>
 
-            <input type="text" 
-            className="border border-slate-400 outline-slate-400 px-4 py-2 rounded-md" placeholder="Digite a descrição da tarefa" 
+            <Input 
+            type="text" 
+            placeholder="Digite a descrição da tarefa" 
             value={description} 
             onChange={(event) => setDescription(event.target.value)}/>
 
